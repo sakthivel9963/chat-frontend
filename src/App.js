e3b0cb49dc9e1f235f7fa93f305f7f2e7095d74a
sakthivel9963 <sakthivel9963@gmail.com>
2019-11-21 02:11:46 +0530

    :tada: :smile: connection has been established been client and server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
-
+import io from "socket.io-client";
+import Axios from "axios";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "http://localhost:5000"
+    };
+  }
+
+  componentDidMount() {
+    const { url } = this.state;
+    const socket = io(url);
+    socket.on("connected", data => console.log(data));
+    // this.getData();
+  }
+
+  getData = async () => {
+    const { url } = this.state;
+    const data = await Axios.get(url);
+    console.log(data);
+  };
+
   render() {
     return (
       <React.Fragment>
